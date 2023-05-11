ca52316b2268ef257781706580bc798624f0d7ec
Sean Zweifel <spzweifel@gmail.com>
2023-05-11 18:50:09 -0400

    almost finished the seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,20 +1,32 @@
 --in here, create the tables. include a drop database if exists
-create TABLE department (
+--I think this is finished but double check it later on
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE department(
     id INT PRIMARY KEY,
     name VARCHAR(30)
 );
 
-create table roles (
-    id INT NOT NULL,
+CREATE TABLE roles(
+    id INT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
 );
 
-create table employee (
+CREATE TABLE employee(
     id INT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT
-);
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id),
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+); 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,19 @@
 --put the inserts here with all of the values like employee name and such
+INSERT INTO department (id, name)
+VALUES (1, "engineering"),
+       (2, "sales"),
+       (3, "finance"),
+       (4, "legal"),
+       (5, "management");
+
+INSERT INTO roles (id, title, salary, department_id)
+VALUES (1, "engineer", 80000, "engineering"),
+       (2, "salesman", 120000, "sales"),
+       (3, "financier", 60000, "finance"),
+       (4, "lawyer", 90000, "legal"),
+       (5, "manager", 200000, "management");
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (1, "Ben", "Ledoux", 1, )
+
+--join on a manager table and excluding the id
