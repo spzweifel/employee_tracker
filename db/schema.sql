e170f2eb40a732bf2d0f7db1ce23785adc66171a
Sean Zweifel <spzweifel@gmail.com>
2023-06-02 17:55:59 -0400

    added to the else if statement

--- db/schema.sql
+++ db/schema.sql
@@ -6,12 +6,12 @@ CREATE DATABASE company_db;
 USE company_db;
 
 CREATE TABLE department(
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(30)
 );
 
 CREATE TABLE roles(
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
@@ -20,7 +20,7 @@ CREATE TABLE roles(
 );
 
 CREATE TABLE employee(
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
